b4086f4ed2178eb527657fd2c8ab87b4f6abd22e
KNA <kristian1711@gmail.com>
2019-10-21 04:31:07 +0200

    First setup of Main and Game pages 'logic' package created, but still empty

--- app/src/main/java/com/example/galgeleg/MainActivity.java
+++ app/src/main/java/com/example/galgeleg/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.galgeleg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GamePage.class);
+        startActivity(intent);
     }
 }
