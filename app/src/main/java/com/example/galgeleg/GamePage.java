11d36cffefdb4bd647dc908a7dc7587c2ae426f5
KNA <kristian1711@gmail.com>
2019-10-21 05:18:47 +0200

    Deleted 'GRAFIK' package and moved every .png to drawable

--- app/src/main/java/com/example/galgeleg/GamePage.java
+++ app/src/main/java/com/example/galgeleg/GamePage.java
@@ -3,12 +3,24 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import logic.Galgelogik;
 
 public class GamePage extends AppCompatActivity {
+    ImageView hangman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
+        hangman = (ImageView) findViewById(R.id.hangman);
+
+        newGame();
+    }
+
+    private void newGame() {
+        hangman.setImageResource(R.drawable.galge);
+        Galgelogik game = new Galgelogik();
     }
 }
